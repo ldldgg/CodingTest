2d7e353e98e2ca0724af98428c95eb017bac080a
dong <dong@DESKTOP-VLESJ3L>
2022-07-27 22:51:03 +0900

    코테추가

---
+++ CodingTest/src/onelevel/StringDownSort.java
@@ -0,0 +1,30 @@
+package onelevel;
+
+public class StringDownSort {
+
+    public String solution(String s) {
+        String answer = "";
+        
+        char[] arr = s.toCharArray();
+        char temp = ' ';
+        
+        for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				if(arr[i] < arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+        
+        for (int i = 0; i < arr.length; i++) {
+			answer += arr[i];
+		}
+        
+        
+        
+        return answer;
+    }
+	
+}
---
+++ CodingTest/src/onelevel/StringToNum.java
@@ -0,0 +1,19 @@
+package onelevel;
+
+public class StringToNum {
+
+	public int solution(String s) {
+		int answer = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+				answer += Math.pow(10, s.length() - i - 1) * (s.charAt(i) - '0');
+			}
+		}
+		if (s.charAt(0) == '-') {
+			answer = answer * -1;
+		}
+		return answer;
+	}
+
+}
