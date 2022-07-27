97dff86d6ba60d383ef10bbb7bcf6aa2beedb03e
dong <dong@DESKTOP-VLESJ3L>
2022-07-27 10:57:58 +0900

    코테 추가

--- CodingTest/src/onelevel/Scissor.java
+++ CodingTest/src/onelevel/Scissor.java
@@ -3,27 +3,28 @@ package onelevel;
 public class Scissor {
 
 	public String solution(String s, int n) {
-        String answer = "";
-        
-        int temp = 0;
-        
-        
-        for (int i= 0; i < s.length(); i++) {
-        	temp = s.charAt(i);
-        	
-        	if(temp == 32) {
-        		
-        	}
-        	if(temp >= 65 && temp <=94) {
-        		temp += n;
-        	}
-        	
-        	
-        	
+		String answer = "";
+
+		int temp = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			temp = s.charAt(i);
+
+			if (temp >= 65 && temp <= 90) {
+				temp += n;
+				if (temp >= 91) {
+					temp -= 26;
+				}
+			} else if (temp >= 97 && temp <= 122) {
+				temp += n;
+				if (temp >= 123) {
+					temp -= 26;
+				}
+			}
+
+			answer += (char) temp;
 		}
-        
-        
-        
-        return answer;
-    }
+
+		return answer;
+	}
 }
